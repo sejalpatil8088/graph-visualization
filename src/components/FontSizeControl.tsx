21bef731367a2dd6b4ac70d5ff376dc095534094
sejal patil <119742317+sejalpatil8088@users.noreply.github.com>
2025-02-05 17:05:52 +0530

    Update FontSizeControl.tsx

--- src/components/FontSizeControl.tsx
+++ src/components/FontSizeControl.tsx
@@ -8,10 +8,7 @@ const FontSizeControl = () => {
   const dispatch = useDispatch();
   const node = useSelector((state: RootState) => state.graph.find(n => n.id === "node-1"));
 
-//   const handleFontSizeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     if (!node) return;
-//     dispatch(updateNode({ id: "node-1", data: { ...node.data, fontSize: parseInt(event.target.value) } }));
-//   };
+
 const handleFontSizeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (!node) return;
     dispatch(updateNode({ 
@@ -33,24 +30,3 @@ export default FontSizeControl;
 
 
 
-// import React from "react";
-// import { useDispatch } from "react-redux";
-// import { updateNode } from "../redux/graphSlice";
-
-// const FontSizeControl = () => {
-//   const dispatch = useDispatch();
-
-//   const handleFontSizeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     const fontSize = parseInt(event.target.value);
-//     dispatch(updateNode({ id: "node-1", data: { fontSize } }));
-//   };
-
-//   return (
-//     <div>
-//       <label>Font Size:</label>
-//       <input type="range" min="12" max="24" step="1" onChange={handleFontSizeChange} />
-//     </div>
-//   );
-// };
-
-// export default FontSizeControl;
